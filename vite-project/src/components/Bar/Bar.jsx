bfc0d5ed0edde32a8c24155c52a49f3e68ea79e8
Tochka <tochka114@gmail.com>
2025-02-21 23:54:44 +0200

    up

--- vite-project/src/components/Bar/Bar.jsx
+++ vite-project/src/components/Bar/Bar.jsx
@@ -1,25 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import BarStats from "./BarStats";
 import BarBtn from "./BarBtn";
 import Modal from "../Modal/Modal";
 import { Counter } from "../Counter";
+const bottles = {
+  beer: 0,
+  wine: 0,
+  whiskey: 0,
+};
 
 const Bar = ({ count }) => {
-  const [bottles, setBottles] = useState({
-    beer: 0,
-    wine: 0,
-    whiskey: 0,
+  const [bottles, setBottles] = useState(() => {
+    const val = localStorage.getItem("bottles");
+    const parseVal = JSON.parse(val) ?? bottles;
+    return parseVal;
   });
+  useEffect(() => {
+    const stringified = JSON.stringify(bottles);
+    localStorage.setItem("bottles", stringified);
+  }, [bottles]);
   const [isOpenModal, setIsOpenModal] = useState(false);
   const isOpen = () => setIsOpenModal(true);
   const isClose = () => setIsOpenModal(false);
   const addDrink = (drink) =>
     setBottles({ ...bottles, [drink]: bottles[drink] + 1 });
-
+  const resetBottles = () =>
+    setBottles({
+      beer: 0,
+      wine: 0,
+      whiskey: 0,
+    });
   return (
     <div>
       <BarStats bottles={bottles} />
-      <BarBtn addDrink={addDrink} />
+      <BarBtn addDrink={addDrink} resetBottles={resetBottles} />
       <button type="button" onClick={isOpen}>
         open modal windows
       </button>
